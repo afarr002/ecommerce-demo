4ddb8080c5dfddf960bbbe6c382e50d077bfcca1
aaron <afarrell002@gmail.com>
2023-08-05 15:08:53 -0600

    imported axios library and useState. created functionality to run a post api request to checkout cart through stripe - creating api next

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,11 +1,39 @@
 import CartProduct from "src/components/CartProduct";
 
+import axios from "axios";
+
 import Link from "next/link";
 import { useShoppingCart } from "use-shopping-cart";
+import { useState } from "react";
 
 export default function CartPage() {
-  const { cartCount, cartDetails, clearCart, formattedTotalPrice } =
-    useShoppingCart();
+  const {
+    cartCount,
+    cartDetails,
+    clearCart,
+    formattedTotalPrice,
+    redirectToCheckout,
+  } = useShoppingCart();
+  const [isRedirecting, setRedirecting] = useState(false);
+
+  async function onCheckout() {
+    if (cartCount > 0) {
+      try {
+        setRedirecting(true);
+        const { id } = await axios
+          .post("/api/checkout-sessions", cartDetails)
+          .then((res) => res.data);
+        const result = await redirectToCheckout(id);
+        if (result?.error) {
+          console.log(`Error: ${result}`);
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      } finally {
+        setRedirecting(false);
+      }
+    }
+  }
 
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
@@ -47,8 +75,12 @@ export default function CartPage() {
               Total:{" "}
               <span className="font-semibold">{formattedTotalPrice}</span>
             </p>
-            <button className="border rounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 focus:ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-yellow-500 mt-4 max-w-max">
-              Go to checkout
+            <button
+              disabled={isRedirecting}
+              onClick={onCheckout}
+              className="border rounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 focus:ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-yellow-500 mt-4 max-w-max"
+            >
+              {isRedirecting ? "Redirecting..." : "Go to Checkout"}
             </button>
           </div>
         </div>
